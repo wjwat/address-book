ebe01f9a33f891479a104cc8985242f3aa8d2e14
William Watkins <wjwat@onionslice.org>
2022-01-24 17:57:49 -0600

    Cleaned up code, removed examples
    Co-Authored-By: Kevin Williams <kevin@k9w.org>

--- js/main.js
+++ js/main.js
@@ -9,11 +9,8 @@ AddressBook.prototype.addContact = function(contact) {
   this.contacts[contact.id] = contact;
 };
 
-// Return an ID number that will not collide with a pre-existing ID number.
 AddressBook.prototype.assignId = function() {
-  // aB.currentId += 1;
   this.currentId += 1;
-  // return aB.currentId
   return this.currentId;
 };
 
@@ -35,24 +32,3 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
-
-
-// CREATE ADDRESSBOOK
-let aB = new AddressBook();
-
-// CREATE A CONTACT
-let contact = new Contact("Ada", "Lovelace", "503-555-0100");
-
-let contact2 = new Contact("Dad", "Lovelace", "503-555-0100");
-
-// ADD THAT CONTACT TO OUR ADDRESSBOOK
-aB.addContact(contact);
-aB.addContact(contact2);
-
-// DELETE THE CONTACT WITH ID OF 1
-
-let cloneaB = JSON.parse(JSON.stringify(aB));
-console.log(cloneaB);
-console.log(aB.deleteContact(1));
-console.log(aB.deleteContact(5));
-console.log(aB);
