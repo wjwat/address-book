61fbb966136fc5a62927d2ee4097485c109027da
William Watkins <wjwat@onionslice.org>
2022-01-25 10:59:45 -0600

    Add address to contact object
    Co-Authored-by: Andy Plymate <Andy.Plymate@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -30,11 +30,12 @@ AddressBook.prototype.deleteContact = function(id) {
 };
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber, emailAddress) {
+function Contact(firstName, lastName, phoneNumber, emailAddress, physicalAddress) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
-  this.emailAddress = emailAddress
+  this.emailAddress = emailAddress;
+  this.physicalAddress = physicalAddress;
 }
 
 Contact.prototype.fullName = function() {
@@ -61,6 +62,7 @@ function showContact(contactId) {
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
   $(".email-address").html(contact.emailAddress);
+  $(".physical-address").html(contact.physicalAddress);
   let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
@@ -85,11 +87,13 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var emailAddress = $('input#new-email-address').val();
+    var physicalAddress = $('textarea#new-physical-address').val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $('input#new-email-address').val('');
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, emailAddress);
+    $('input#new-physical-address').val('');
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, emailAddress, physicalAddress);
     console.log(newContact);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
