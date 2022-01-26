6d2c64ecf72bda5dc4facadeaac6c27ae1398522
Will W <wjwat@onionslice.org>
2022-01-26 09:44:13 -0600

    Add address field to our contact object & html form

--- js/scripts.js
+++ js/scripts.js
@@ -30,12 +30,13 @@ AddressBook.prototype.deleteContact = function(id) {
 };
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber, emailAddress, nickName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-  this.emailAddress = emailAddress;
-  this.nickName = nickName;
+function Contact(firstName, lastName, phoneNumber, emailAddress, nickName, address) {
+  this.firstName = firstName || '';
+  this.lastName = lastName || '';
+  this.phoneNumber = phoneNumber || '';
+  this.emailAddress = emailAddress || '';
+  this.nickName = nickName || '';
+  this.address = address || [];
 }
 
 Contact.prototype.fullName = function() {
@@ -45,22 +46,62 @@ Contact.prototype.fullName = function() {
 // User Interface Logic ---------
 let addressBook = new AddressBook();
 
+function populateContact() {
+  console.log('hola!');
+  let newContact = new Contact;
+
+  $('form input').each((i, e) => {
+    newContact[$(e).attr('name')] = $(e).val();
+  })
+
+  // We should probably move each address into a div of class address
+  // so when we grab each address we can pull in the sibling addressNickname
+  // to associate it with that.
+  $('.address').each((i, e) => {
+
+    let t = [];
+
+
+    // console.log($(e).find('input'));
+    // console.log($(e).find('textarea'));
+
+    t.push($(e).find('input[name=addressNickname]')[0].value);
+    t.push($(e).find('textarea[name=address]')[0].value);
+
+    newContact['address'].push(t);
+  })
+
+  addressBook.addContact(newContact);
+  console.log(addressBook);
+}
+
 $(document).ready(function() {
-  $('#add-contact').on('click', () => {
-    let newContact = new Contact;
+  $('#add-contact').on('click', populateContact);
+  // $('#add-contact').on('click', () => {
+  //   let newContact = new Contact;
 
-    $('form input').each((i, e) => {
-      newContact[$(e).attr('name')] = $(e).val();
-    })
+  //   $('form input').each((i, e) => {
+  //     newContact[$(e).attr('name')] = $(e).val();
+  //   })
 
-    // s = ['h', 'e', 'l', 'l', 'o']
-    // s.forEach((e, i) => {
-    //   // e === 'h'
-    //   // i === 0
-    //   // s[i] === 'h' is true
-    // })
+  //   // We should probably move each address into a div of class address
+  //   // so when we grab each address we can pull in the sibling addressNickname
+  //   // to associate it with that.
+  //   $('.address').each((i, e) => {
 
-    addressBook.addContact(newContact);
-    console.log(addressBook);
-  });
+  //     let t = [];
+
+
+  //     // console.log($(e).find('input'));
+  //     // console.log($(e).find('textarea'));
+
+  //     t.push($(e).find('input[name=addressNickname]')[0].value);
+  //     t.push($(e).find('textarea[name=address]')[0].value);
+
+  //     newContact['address'].push(t);
+  //   })
+
+  //   addressBook.addContact(newContact);
+  //   console.log(addressBook);
+  // });
 });
